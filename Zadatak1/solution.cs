b7c5b0e63d68d938372bceede976d42fa8a48721
Faruk-Ratkovic <faruk-r-2004@hotmail.com>
2024-10-20 20:56:08 +0200

    Create solution.cs

---
+++ Zadatak1/solution.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        public static int countOcurrences(string sentence, string word)
+        {
+            string[] words = sentence.Split(" ");
+            int count = 0;
+            for (int i = 0; i < words.Count(); i++)
+            {
+                if (words[i]==word)
+                {
+                    count++;
+                }
+                
+            }
+            string? sentence2="";
+            for (int i = 0; i < words.Count(); i++)
+            {
+                
+                sentence2 += words[i];
+                sentence2 += " ";
+            }
+            Console.WriteLine(sentence2);
+                return count;
+        }
+        public static void Main()
+        {
+            string? sentence;
+            Console.WriteLine("Hello, please enter a sentence");
+           
+            sentence = Console.ReadLine();
+            Console.WriteLine("Now please enter a word you want me to find");
+            string? word;
+            word= Console.ReadLine();
+
+            if (sentence != null && word != null) 
+            {
+                Console.WriteLine("In the sentence: " + sentence + " word " + word + " can be found " + countOcurrences(sentence, word) + " times ");
+            }
+           
+        }
+    }
+}
